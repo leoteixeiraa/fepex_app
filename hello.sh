5fc01fa159e69fb4f8e1227773128c1a83585a09
Leonardo Teixeira <leovenancio00@gmail.com>
2019-05-22 16:42:10 -0300

    Arquivo Padrão - Leia me Subido

---
+++ hello.sh
@@ -0,0 +1,9 @@
+                                                                                                                                                                                                                                             
+#!/bin/bash
+clear;
+
+
+
+echo "Hello World";
+
+exit;
