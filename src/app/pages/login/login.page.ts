0eba3df4bc2309a08e7e7ceb8241d1c023e25856
Leonardo Teixeira <leovenancio00@gmail.com>
2019-08-31 02:22:36 -0300

    Finalizando Tela Login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { Keyboard } from '@ionic-native/keyboard/ngx'
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  @ViewChild(IonSlides, {static:false}) slides: IonSlides;
+  public wavesPosition: number = 0;
+  public wavesDifference: number = 85;
 
-  constructor() { }
+  constructor(public keyboard: Keyboard) { }
 
   ngOnInit() {
+    this.keyboard.isVisible
+   }
+
+  segmentChanged(event: any){
+    if (event.detail.value == "login") {
+      this.slides.slidePrev();
+      this.wavesPosition += this.wavesDifference;
+    }else{
+      this.slides.slideNext();
+      this.wavesPosition -= this.wavesDifference;
+      
+    }
+    }
+    
   }
 
-}
+
