b51898d426fc6892f16dfc2c36813340fd9e3b20
Leonardo Teixeira <leovenancio00@gmail.com>
2019-09-19 03:55:26 -0300

    Adicionando scss nas paginas details

---
+++ src/app/interfaces/oficina.ts
@@ -0,0 +1,18 @@
+export interface Oficina {
+    id?: string;
+    title?: string;
+    date?: string;
+    authors?: string;
+    description?: string;
+    short_description?: string;
+    time_start?: string;
+    time_end?: string;
+    vacancies?: string; //vagas
+    status?: string;
+    picture?: string;
+    advisors?: string; //orientadores
+    local?: string;
+    observation?: string;
+    createdAt?: number;
+    userId?: string;
+}
