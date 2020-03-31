3bcc08c1cbdce69a6cce7225b416c33b940decd4
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2020-03-31 14:23:44 -0300

    modificações na tela sobre

---
+++ src/app/interfaces/equipe.ts
@@ -0,0 +1,7 @@
+export interface Equipe {
+    id?: string;
+    nome?: string;
+    funcao?: string;
+    lattes?: string;
+    avatar?: string;
+}
--- src/app/pages/sobre/sobre.page.ts
+++ src/app/pages/sobre/sobre.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Equipe } from 'src/app/interfaces/equipe';
 
 @Component({
   selector: 'app-sobre',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobrePage implements OnInit {
 
-  constructor() { }
+  // private equipe: Equipe = {};
+  private lista: Array<Equipe> = [];
 
+  constructor() {
+    this.lista.push({id: '1', nome: 'Fabricio Bizotto', funcao: 'Professor Colaborador', avatar: '/assets/img/fabricio.gif', lattes: ''})
+    this.lista.push({id: '2', nome: 'Gabriella Aparecida Ronchi', funcao: 'Aluna Colaboradora', avatar: '/assets/img/fabricio.gif', lattes: ''})
+    this.lista.push({id: '3', nome: 'Fabricio', funcao: 'Professor Colaborador', avatar: '/assets/img/fabricio.gif', lattes: ''})
+    this.lista.push({id: '4', nome: 'Fabricio', funcao: 'Professor Colaborador', avatar: '/assets/img/fabricio.gif', lattes: ''})
+    this.lista.push({id: '5', nome: 'Fabricio', funcao: 'Professor Colaborador', avatar: '/assets/img/fabricio.gif', lattes: ''})
+    this.lista.push({id: '6', nome: 'Fabricio', funcao: 'Professor Colaborador', avatar: '/assets/img/fabricio.gif', lattes: ''})
+   }
+
+  // chama este método ao carregar a tela
   ngOnInit() {
+    
+
+    // console.log(this.lista)
   }
 
 }
